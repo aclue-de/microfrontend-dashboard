8ff9351c4b94164819424f9b3440182fcbe0f88c
Robin Müller <robin.mueller@aclue.de>
2021-11-05 11:43:31 +0100

    add className prefix

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { createStyles, Grid, makeStyles, ThemeProvider, Typography } from '@material-ui/core';
+import { createGenerateClassName, createStyles, Grid, makeStyles, StylesProvider, ThemeProvider, Typography } from '@material-ui/core';
 import { Adbox, HelpButton, KeyFigures, Newsbox } from '../components';
 import { theme } from '../theme/theme';
 const useStartStyles = makeStyles((theme) => createStyles({
@@ -18,7 +18,10 @@ const useStartStyles = makeStyles((theme) => createStyles({
         marginLeft: 'auto',
     },
 }));
+const generateClassName = createGenerateClassName({
+    seed: 'startpage',
+});
 export const Start = () => {
     const classes = useStartStyles();
-    return (_jsx(ThemeProvider, Object.assign({ theme: theme }, { children: _jsxs("div", Object.assign({ className: classes.root }, { children: [_jsxs("div", Object.assign({ className: classes.headerContainer }, { children: [_jsx(Typography, Object.assign({ variant: "h1", className: classes.title }, { children: "\u00DCbersicht" }), void 0), _jsx(HelpButton, { className: classes.helpButton }, void 0)] }), void 0), _jsx(KeyFigures, {}, void 0), _jsxs(Grid, Object.assign({ spacing: 1, container: true }, { children: [_jsx(Grid, Object.assign({ item: true, xs: 12, sm: 6, md: 9, lg: 9 }, { children: _jsx(Newsbox, {}, void 0) }), void 0), _jsx(Grid, Object.assign({ item: true, xs: 12, sm: 6, md: 3, lg: 3 }, { children: _jsx(Adbox, {}, void 0) }), void 0)] }), void 0)] }), void 0) }), void 0));
+    return (_jsx(StylesProvider, Object.assign({ generateClassName: generateClassName }, { children: _jsx(ThemeProvider, Object.assign({ theme: theme }, { children: _jsxs("div", Object.assign({ className: classes.root }, { children: [_jsxs("div", Object.assign({ className: classes.headerContainer }, { children: [_jsx(Typography, Object.assign({ variant: "h1", className: classes.title }, { children: "\u00DCbersicht" }), void 0), _jsx(HelpButton, { className: classes.helpButton }, void 0)] }), void 0), _jsx(KeyFigures, {}, void 0), _jsxs(Grid, Object.assign({ spacing: 1, container: true }, { children: [_jsx(Grid, Object.assign({ item: true, xs: 12, sm: 6, md: 9, lg: 9 }, { children: _jsx(Newsbox, {}, void 0) }), void 0), _jsx(Grid, Object.assign({ item: true, xs: 12, sm: 6, md: 3, lg: 3 }, { children: _jsx(Adbox, {}, void 0) }), void 0)] }), void 0)] }), void 0) }), void 0) }), void 0));
 };
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createStyles, Grid, makeStyles, Theme, ThemeProvider, Typography } from '@material-ui/core'
+import { createGenerateClassName, createStyles, Grid, makeStyles, StylesProvider, Theme, ThemeProvider, Typography } from '@material-ui/core'
 import { Adbox, HelpButton, KeyFigures, Newsbox } from '../components'
 import { theme } from '../theme/theme'
 
@@ -22,28 +22,34 @@ const useStartStyles = makeStyles((theme: Theme) =>
     }),
 )
 
+const generateClassName = createGenerateClassName({
+    seed: 'startpage',
+})
+
 export const Start: React.FC = () => {
     const classes = useStartStyles()
 
     return (
-        <ThemeProvider theme={theme}>
-                <div className={classes.root}>
-                    <div className={classes.headerContainer}>
-                        <Typography variant="h1" className={classes.title}>
-                            Übersicht
-                        </Typography>
-                        <HelpButton className={classes.helpButton} />
-                    </div>
-                    <KeyFigures />
-                    <Grid spacing={1} container>
-                        <Grid item xs={12} sm={6} md={9} lg={9}>
-                            <Newsbox />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={3} lg={3}>
-                            <Adbox />
-                        </Grid>
-                    </Grid>
-                </div>
-        </ThemeProvider>
+        <StylesProvider generateClassName={generateClassName}>
+            <ThemeProvider theme={theme}>
+                            <div className={classes.root}>
+                                <div className={classes.headerContainer}>
+                                    <Typography variant="h1" className={classes.title}>
+                                        Übersicht
+                                    </Typography>
+                                    <HelpButton className={classes.helpButton} />
+                                </div>
+                                <KeyFigures />
+                                <Grid spacing={1} container>
+                                    <Grid item xs={12} sm={6} md={9} lg={9}>
+                                        <Newsbox />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                                        <Adbox />
+                                    </Grid>
+                                </Grid>
+                            </div>
+                    </ThemeProvider>
+        </StylesProvider>
     )
 }
