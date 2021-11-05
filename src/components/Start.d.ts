0e9414676ce24b70768040f977791c7506a8e620
Robin Müller <robin.mueller@aclue.de>
2021-11-05 10:12:38 +0100

    build startpage

---
+++ src/components/Start.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import './Start.css';
+export declare const Start: () => JSX.Element;
---
+++ src/components/Start.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import './Start.css';
+export const Start = () => {
+    return (_jsx("div", Object.assign({ className: "App" }, { children: _jsxs("header", Object.assign({ className: "App-header" }, { children: [_jsxs("p", { children: ["Edit ", _jsx("code", { children: "src/App.tsx" }, void 0), " and save to reload."] }, void 0), _jsx("a", Object.assign({ className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, { children: "Hallo Workshop" }), void 0)] }), void 0) }), void 0));
+};
