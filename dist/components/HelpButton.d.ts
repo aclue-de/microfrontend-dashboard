e367e000b3bf1b11526455c0d49d6770988b11c8
Robin Müller <robin.mueller@aclue.de>
2022-04-11 16:32:12 +0200

    remove index.css

--- dist/components/HelpButton.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { IconButtonProps } from '@mui/material';
-export declare const HelpButton: React.FC<IconButtonProps>;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import './index.css';
-import { Start } from './pages/Start';
-export { Start };
